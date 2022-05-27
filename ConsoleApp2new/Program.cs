fe1625039ff325a22ca1b0f3facf19e7bb13df22
tuncay1998 <tunjayjn@code.edu.az>
2022-05-27 13:53:48 +0400

    file created

---
+++ ConsoleApp2new/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] listofnumbers = { 6, 9, 12 };
+            int number = 3;
+            newNumber(listofnumbers, 3);
+
+        }
+
+        static void newNumber(int[] listofnumbers, int number)
+        {
+            int[] newone = new int[listofnumbers.Length + 1];
+            for (int i = 0; i < listofnumbers.Length; i++)
+            {
+                newone[i] = listofnumbers[i];
+            }
+            newone[new.Length - 1] = number;
+        }
+
+
+    }
+}
